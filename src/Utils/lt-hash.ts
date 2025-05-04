75e37cd9f53379284cb6906c38d4e2df77b222ca
Shizo Devs <codeX.shizothetechie@gmail.com>
2025-05-04 09:04:38 +0530

    Update lt-hash.ts

--- src/Utils/lt-hash.ts
+++ src/Utils/lt-hash.ts
@@ -1,53 +1,61 @@
 import { hkdf } from './crypto'
 
+/**
+ * LT Hash is a summation based hash algorithm that maintains the integrity of a piece of data
+ * over a series of mutations. You can add/remove mutations and it'll return a hash equal to
+ * if the same series of mutations was made sequentially.
+ */
+
 const o = 128
 
 class d {
+
 	salt: string
 
 	constructor(e: string) {
 		this.salt = e
 	}
-
-	async add(e, t) {
-		for (const item of t) {
-			e = await this._addSingle(e, item)
+	add(e, t) {
+		var r = this
+		for(const item of t) {
+			e = r._addSingle(e, item)
 		}
+
 		return e
 	}
-
-	async subtract(e, t) {
-		for (const item of t) {
-			e = await this._subtractSingle(e, item)
+	subtract(e, t) {
+		var r = this
+		for(const item of t) {
+			e = r._subtractSingle(e, item)
 		}
+
 		return e
 	}
-
-	async subtractThenAdd(e, subtractList, addList) {
-		const subtracted = await this.subtract(e, subtractList)
-		return await this.add(subtracted, addList)
+	subtractThenAdd(e, t, r) {
+		var n = this
+		return n.add(n.subtract(e, r), t)
 	}
-
 	async _addSingle(e, t) {
-		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: this.salt })).buffer
-		return this.performPointwiseWithOverflow(await e, n, (a, b) => a + b)
+		var r = this
+		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: r.salt })).buffer
+		return r.performPointwiseWithOverflow(await e, n, ((e, t) => e + t))
 	}
-
 	async _subtractSingle(e, t) {
-		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: this.salt })).buffer
-		return this.performPointwiseWithOverflow(await e, n, (a, b) => a - b)
-	}
+		var r = this
 
+		const n = new Uint8Array(await hkdf(Buffer.from(t), o, { info: r.salt })).buffer
+		return r.performPointwiseWithOverflow(e, n, ((e, t) => e - t))
+	}
 	performPointwiseWithOverflow(e, t, r) {
-		const n = new DataView(e),
-			i = new DataView(t),
-			a = new ArrayBuffer(n.byteLength),
-			s = new DataView(a)
-		for (let e = 0; e < n.byteLength; e += 2) {
-			s.setUint16(e, r(n.getUint16(e, true), i.getUint16(e, true)), true)
+		const n = new DataView(e)
+		  , i = new DataView(t)
+		  , a = new ArrayBuffer(n.byteLength)
+		  , s = new DataView(a)
+		for(let e = 0; e < n.byteLength; e += 2) {
+			s.setUint16(e, r(n.getUint16(e, !0), i.getUint16(e, !0)), !0)
 		}
+
 		return a
 	}
 }
-
 export const LT_HASH_ANTI_TAMPERING = new d('WhatsApp Patch Integrity')
