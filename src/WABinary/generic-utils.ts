03087f87b7ad860266c9a55c49012c9f0de707bc
Yugesh Singh <Yugeshsingh27@gmail.com>
2025-05-30 08:46:40 +0530

    Major Update...

--- src/WABinary/generic-utils.ts
+++ src/WABinary/generic-utils.ts
@@ -1,6 +1,6 @@
 import { Boom } from '@hapi/boom'
 import { proto } from '../../WAProto'
-import { BinaryNode } from './types'
+import { BinaryNode } from './Types'
 
 // some extra useful utilities
 
