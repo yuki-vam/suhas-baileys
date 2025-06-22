f7ff6c99dc36dc5ad4314a7559440c8ef8a0ac18
AmilaPrabathKumara <Amilaprabath0762280384v2@gmail.com>
2025-06-22 17:07:37 +0530

    Update messages-send.ts

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs';
+import { createReadStream } from 'fs';
 import { Boom } from '@hapi/boom'
 import NodeCache from '@cacheable/node-cache'
 import { Readable } from 'stream'
