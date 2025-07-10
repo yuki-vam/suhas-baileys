fbe5f9636fc4223f7a3e2464ed85cfb074186ba3
Zara Zara <183077950+md2839pv404@users.noreply.github.com>
2025-07-10 12:39:30 +0530

    Update messages-send.ts

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -347,7 +347,8 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 			deviceSentMessage: {
 				destinationJid,
 				message
-			}
+			},
+			messageContextInfo: message.messageContextInfo
 		}
 
 		const extraAttrs = {}
